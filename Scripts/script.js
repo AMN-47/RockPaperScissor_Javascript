fdde7fbadb30cf19a893039f37950d34f46b49bc
AMN-47 <akhilnagar@mweb.co.za>
2022-12-07 19:33:28 +0200

    add Scripts/script.js

--- Scripts/script.js
+++ Scripts/script.js
@@ -3,6 +3,7 @@ const usChoiceDis = document.getElementById('PLAYER-CHOICE')
 const showRes = document.getElementById('daRESULT')
 const possibleChoices = document.querySelectorAll('button')
 let usChoice
+let comChoice
 
 possibleChoices.forEach(possibleChoices => possibleChoices.addEventListener('click', (e) =>{
     usChoice = e.target.id
@@ -11,6 +12,18 @@ possibleChoices.forEach(possibleChoices => possibleChoices.addEventListener('cli
 }))
 
 function generateCompChoice(){
-    const randoNum = Math.floor(Math.random() * 3) 
+    const randoNum = Math.floor(Math.random() * 3) + 1
     console.log(randoNum)
+
+    if (randoNum === 1){
+        comChoice = 'ROCK'
+    }
+    if (randoNum === 2){
+        comChoice = 'scissors'
+    }
+    if (randoNum === 3){
+        comChoice = 'paper'
+    }
+    compChoiceDis.innerHTML = comChoice
+
 }
